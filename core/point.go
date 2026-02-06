be11a6cff3888eccf9dab501ec4682f5929ac177
lateshmunde <latesh.munde@cctech.co.in>
2026-02-06 10:14:38 +0530

    point modified : ISEqualPoint method

--- core/point.go
+++ core/point.go
@@ -21,3 +21,10 @@ func (p1 Point) Distance(p2 Point) float64 {
 func (p1 Point) Subtraction(p2 Point) Point {
 	return Point{(p1.X - p2.X), (p1.Y - p2.Y), (p1.Z - p2.Z)}
 }
+
+func (p1 Point) IsEqualPoint(p2 Point) bool {
+	if math.Abs(p1.X-p2.X) < 10e-6 && math.Abs(p1.Y-p2.X) < 10e-6 && math.Abs(p1.Z-p2.Z) < 10e-6 {
+		return true
+	}
+	return false
+}
